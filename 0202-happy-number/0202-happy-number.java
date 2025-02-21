147db91a7b1b6ef3b85e102a5856a0db513a2de0
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2025-02-21 23:29:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (74.19%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,14 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n == 1){
-          return true;
-        }
-        int slow = calculate(n);
-        int fast = calculate(slow);
-        int temp=0;
-        while(fast !=10 && fast != 100){
-          slow = calculate(slow);
-          temp = calculate(fast);
-          if(temp ==1 || temp ==10 || temp == 100){
-            break;
-          }
-          fast  = calculate(temp);
-          if(slow == fast){
-            return false;
-          }
-        }
-        return true;
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = calculate(slow);           // Move slow pointer one step
+            fast = calculate(calculate(fast)); // Move fast pointer two steps
+        } while (slow != fast);              // Detect cycle
+
+        return slow == 1; 
     }
 
     public static int calculate(int n){
