e98cbc6a13dc19954233ef6fed83cd1efefcd433
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-06-19 14:35:28 +0530

    Time: 1 ms (93.94%) | Memory: 43.3 MB (69.47%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 class Solution {
 
-    List<List<Integer>> pascalTri ;
-
-    public Solution(){
-        pascalTri = new ArrayList<>();
-        pascalTri.add(Arrays.asList(1));
-        pascalTri.add(Arrays.asList(1,1));
-
-        for(int i=2;i<31;i++){
-            List<Integer> temp = new ArrayList<>();
-            temp.add(1);
-            for(int j=1;j<i;j++){
-                temp.add(pascalTri.get(i-1).get(j-1)+pascalTri.get(i-1).get(j));
-            }
-            temp.add(1);
-            pascalTri.add(temp);
-        }
-
-    }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
+        // ans.add(Arrays.asList(1));
+        // if(numRows ==1){
+        //     return ans;
+        // }
+        //ans.add(Arrays.asList(1,1));
 
         for(int i=0;i<numRows;i++){
-            ans.add(pascalTri.get(i));
+            List<Integer> temp = new ArrayList<>();
+            
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    temp.add(1);
+                }else{
+                    temp.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+                }
+            }
+            ans.add(temp);
         }
+
+        //System.out.println("numRows = "+numRows+" size = "+ans.size());
 
         return ans;
     }
