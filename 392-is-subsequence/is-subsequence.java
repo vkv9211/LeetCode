52db6abc80915ab86fbee99c5f546a439ef9f77f
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-06-26 15:58:08 +0530

    Time: 1 ms (95.32%) | Memory: 42.7 MB (79.34%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -3,27 +3,37 @@ class Solution {
         int sLen = s.length();
         int tLen = t.length();
 
-        return isSubsequenceUtil(s, sLen-1, t,  tLen-1);
+        int sStart = 0;
+        int tStart = 0;
+
+        while(sStart<sLen && tStart<tLen){
+            if(s.charAt(sStart)==t.charAt(tStart)){
+                sStart++;
+            }
+            tStart++;
+        }
+
+        return sStart == sLen;
     }
 
-    public boolean isSubsequenceUtil(String s, int sLen, String t, int tLen){
-        System.out.println("sLen = "+sLen+",tLen = "+tLen);
-        if(sLen <0){
-            return true;
-        }
-        if(tLen <0){
-            return false;
-        }
+    // public boolean isSubsequenceUtil(String s, int sLen, String t, int tLen){
+    //     System.out.println("sLen = "+sLen+",tLen = "+tLen);
+    //     if(sLen <0){
+    //         return true;
+    //     }
+    //     if(tLen <0){
+    //         return false;
+    //     }
 
-        boolean match = false;
-        boolean noMatch = false;
+    //     boolean match = false;
+    //     boolean noMatch = false;
 
-        if(s.charAt(sLen) == t.charAt(tLen)){
-            match = isSubsequenceUtil(s, sLen-1, t, tLen-1);
-        }else{
-            noMatch = isSubsequenceUtil(s, sLen, t, tLen-1);
-        }
+    //     if(s.charAt(sLen) == t.charAt(tLen)){
+    //         match = isSubsequenceUtil(s, sLen-1, t, tLen-1);
+    //     }else{
+    //         noMatch = isSubsequenceUtil(s, sLen, t, tLen-1);
+    //     }
 
-        return match || noMatch;
-    }
+    //     return match || noMatch;
+    // }
 }
