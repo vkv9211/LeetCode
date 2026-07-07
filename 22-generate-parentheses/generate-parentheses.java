93eab291990f2315d684f1302c0fec2c93168bae
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-07-07 13:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (99.17%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -6,23 +6,30 @@ class Solution {
         // if(n == 1){
         //     return new ArrayList<>().add("()");
         // }
-        Set<String> set  = new HashSet<>();
+        List<String> set  = new ArrayList<>();
 
-        util("", 0, 0, n, set);
+        util(new StringBuilder(), 0, 0, n, set);
 
         return new ArrayList<>(set);
     }
 
-    public void util(String sb, int start, int end, int n, Set<String> set){
+    public void util(StringBuilder sb, int start, int end, int n, List<String> set){
        // System.out.println("starting sb = "+sb);
         if(start == n && end == n){
           //  System.out.println("sb = "+sb);
-            set.add(sb);
+            set.add(sb.toString());
         }
-        if(start > n || end > n || end > start){
-            return ;
+        
+        if(start < n){
+            sb.append("(");
+            util(sb, start+1, end, n, set);
+            sb.deleteCharAt(sb.length()-1);
         }
-        util(sb+"(", start+1, end, n, set);
-        util(sb+")", start, end+1, n, set);
+        if(end < start){
+            sb.append(")");
+            util(sb, start, end+1, n, set);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        
     }
 }
