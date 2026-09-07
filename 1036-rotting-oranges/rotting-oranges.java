1a45b15024a549d2452067643b55460ba08e6463
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-09-07 11:24:11 +0530

    Time: 10 ms (3.31%) | Memory: 44.5 MB (13.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,25 +2,33 @@ import java.util.*;
 
 class Solution {
 
-    int count = 0;
+    
     public int orangesRotting(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
 
         Queue<int[]> q = new LinkedList<>();
+        int count = 0;
+        int freshTomato =0;
 
         int[][] dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j] == 2){
-                    System.out.println("i = "+i+", j = "+j);
+                  //  System.out.println("i = "+i+", j = "+j);
                     q.add(new int[]{i,j});
                     
+                }else if(grid[i][j] == 1){
+                    freshTomato++;
                 }
             }
             
         }
+        if(freshTomato == 0 ){
+            return 0;
+        }
+        System.out.println("freshTomato = "+freshTomato);
         q.add(null);
 
         while(!q.isEmpty()){
@@ -35,11 +43,11 @@ class Solution {
                 }
                 continue;
             }
-
+            
             int x = temp[0];
             int y = temp[1];
             
-            System.out.println("x = "+x+", y = "+y);
+          //  System.out.println("x = "+x+", y = "+y);
             for(int i =0;i<4; i++){
                 int new_x = x+dir[i][0];
                 int new_y = y+dir[i][1];
@@ -47,29 +55,17 @@ class Solution {
                 if(new_x <0 ||new_x >= row || new_y<0 || new_y >=col || grid[new_x][new_y] == 0 || grid[new_x][new_y] == 2){
                     continue;
                 }
-                System.out.println("new_x = "+new_x+", new_y = "+new_y+", grid[new_x][new_y] = "+grid[new_x][new_y]);
+               // System.out.println("new_x = "+new_x+", new_y = "+new_y+", grid[new_x][new_y] = "+grid[new_x][new_y]);
                 q.add(new int[]{new_x, new_y});
+                freshTomato--;
                 grid[new_x][new_y] = 2;
                 
             }
             
         }
-
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-            
-        }
-
-        return count;
+        System.out.println("freshTomato = "+freshTomato);
+        return freshTomato == 0 ? count: -1;
     }
 
-    public void bfs(int row, int col, int count, int[][] grid, Queue<int[]> q, int[][] dir){
-        
-
-        
-    }
+    
 }
