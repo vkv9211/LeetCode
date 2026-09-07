61a4161ecb50e05848c9ac1233643a74c56934e6
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-09-07 10:37:28 +0530

    Time: 1 ms (38.40%) | Memory: 47 MB (24.62%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -5,20 +5,22 @@ class Solution {
         int row = image.length;
         int col = image[0].length;
 
+        if(init == color){
+            return image;
+        }
+
         //System.out.println("init = "+init+", color = "+color);
         int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-        boolean[][] dp = new boolean[row][col];
 
-        dfs(sr,sc,color, init,row, col, image, dir, dp);
+        dfs(sr,sc,color, init,row, col, image, dir);
 
         return image;
         
     }
 
-    public void dfs(int sr, int sc, int color, int init, int row, int col, int[][] image, int[][] dir, boolean[][] dp){
+    public void dfs(int sr, int sc, int color, int init, int row, int col, int[][] image, int[][] dir){
        // System.out.println("sr= "+sr+", sc = "+sc+", init = "+init+", color = "+color);
         image[sr][sc] = color;
-        dp[sr][sc] = true;
 
         for(int i=0;i<4;i++){
             int new_x = sr+dir[i][0];
@@ -27,10 +29,7 @@ class Solution {
             if(new_x <0 || new_x >= row || new_y <0 || new_y >= col|| image[new_x][new_y] != init){
                 continue;
             }
-            if(!dp[new_x][new_y]){
-                dfs(new_x, new_y, color, init,row, col, image, dir, dp);
-            }
-            
+            dfs(new_x, new_y, color, init,row, col, image, dir);
         }
     }
 }
