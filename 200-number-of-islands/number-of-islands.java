55f238693a5982ac7af059be966cd0cd9bcd743e
vkv9211 <vikrantkumarvikram.9211@gmail.com>
2026-09-07 09:37:58 +0530

    Time: 3 ms (87.84%) | Memory: 52.2 MB (67.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -11,26 +11,26 @@ class Solution {
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]-'0' ==1 && dp[i][j] == false ){
+                if(grid[i][j]== '1'){
                     num++;
-                    dp(i,j,m,n,grid,dp,dir);
+                    dp(i,j,m,n,grid,dir);
                 }
             }
         }
         return num;
     }
 
-    public void dp(int x, int y, int m, int n, char[][] grid, boolean[][] dp, int[][] dir){
-        dp[x][y] = true;
+    public void dp(int x, int y, int m, int n, char[][] grid,int[][] dir){
+        grid[x][y] = '0';
         for(int i =0;i<4;i++){
             int new_x = x+dir[i][0];
             int new_y = y+dir[i][1];
 
-            if(new_x <0 || new_x >=m || new_y <0 || new_y >= n || grid[new_x][new_y]-'0' == 0){
+            if(new_x <0 || new_x >=m || new_y <0 || new_y >= n || grid[new_x][new_y] == '0'){
                 continue ;
             }
-            if(!dp[new_x][new_y] ){
-                dp(new_x, new_y, m, n,grid, dp, dir );
+            if(grid[new_x][new_y] == '1'){
+                dp(new_x, new_y, m, n,grid, dir );
             }
         }
     }
